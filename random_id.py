a36a0f8bdc5e11f975dfad45c853c8f422b1c6d7
siam-saharear <121116026+siam-saharear@users.noreply.github.com>
2024-01-16 21:30:01 +0600

    Create random_id.py

---
+++ random_id.py
@@ -0,0 +1,17 @@
+import random
+alphabates =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def random_id_generator(n):
+    product_id = ""
+    while len(product_id) != n:
+        step = random.randint(0,3)
+        if step == 1:
+            product_id += str(alphabates[random.randint(1,25)])
+        elif step == 2:
+            product_id += str(alphabates[random.randint(0,5)].lower())
+        elif step == 3:
+            product_id += str(random.randint(0,9))
+    return product_id
+
+i = random_id_generator(18)
+print(i)
