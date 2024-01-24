8bd9d7bf9514679df46bb4742c3443ff4c2d9cd5
siam-saharear <121116026+siam-saharear@users.noreply.github.com>
2024-01-24 18:19:02 +0600

    Update random_id.py

--- random_id.py
+++ random_id.py
@@ -1,15 +1,24 @@
 import random
+import scyjava
 
-def random_id_generator(n=18):
+# def   
+def random_id_generator( n=18,upper= True, lower = True, number = True):
     alphabates =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
     product_id = ""
+
     while len(product_id) != n:
         step = random.randint(0,3)
-        if step == 1:
-            product_id += str(alphabates[random.randint(1,25)])
-        elif step == 2:
+        
+        if step == 1 and upper == True:
+            product_id += str(alphabates[random.randint(1,25)]).upper()
+        
+        elif step == 2 and lower == True:
             product_id += str(alphabates[random.randint(0,5)].lower())
-        elif step == 3:
+        
+        elif step == 3 and number == True:
             product_id += str(random.randint(0,9))
+    
     return product_id
-
+# example
+random_number = random_id_generator(number = False , n = 26)
